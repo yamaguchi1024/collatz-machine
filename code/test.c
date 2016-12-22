7e866ae81d998fe843f46a0377bee891f5fcd7fb
yamaguchi1024 <yukatkh@yahoo.co.jp>
2016-12-22 21:31:40 +0900

    should test in quartus

---
+++ code/test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int cnt = 0;
+    while(n!=1){
+        if(n%2==0) n=n/2;
+        else n=n*3+1;
+        cnt++;
+    }
+    printf("%d\n",cnt);
+    return 0;
+}
--- code/test.vhd
+++ code/test.vhd
@@ -3,41 +3,46 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity gray_counter is
-	port
-	(
-	   data : in integer range 0 to 255;
-		go :in std_logic;
-		clk		: in std_logic;
-		q		: out integer range 0 to 255
-	);
+    port
+    (
+        data : in integer range 0 to 255;
+        go :in std_logic;
+        clk		: in std_logic;
+        q		: out integer range 0 to 255
+    );
 end entity;
 
 architecture rtl of gray_counter is
 begin
-	process (clk)
-		variable	  dreg   : integer range 0 to 13120;
-		variable   counter: integer range 0 to 127;
-	begin
-		if (rising_edge(clk)) then
-		  if go = '1' then
-		    dreg := data;
-			 counter := 0;
-		  end if;
-		end if;
-	   L1: loop
-		   dreg := dreg - 1;
-			exit L1 when (dreg = 1);
-	   end loop L1;
-		q <= counter;
-	end process;
+    process (clk)
+        variable    dreg   : integer range 0 to 13120;
+        variable    counter: integer range 0 to 127;
+        variable    calc   : std_logic;
+        variable    div2   : integer range 0 to 13120;
+        variable    mul3   : integer range 0 to 13120;
+    begin
+        if (rising_edge(clk) and go = '1') then
+            dreg := data;
+            counter := 0;
+            calc := '1';
+            div2 := dreg / 2;
+            mul3 := (dreg * 3) + 1;
+        end if;
 
+        while (dreg /= 1) loop
+            if(rising_edge(clk) and calc = '1') then
+                if dreg mod 2 = 0 then
+                    dreg := div2;
+                else
+                    dreg := mul3;
+                end if;
+
+                div2 := dreg / 2;
+                mul3 := (dreg * 3) + 1;
+
+            end if;
+        end loop;
+
+        q <= counter;
+    end process;
 end rtl;
-
---		   while data='1' loop
---			   cnt := cnt + 1;
---				if data mod 2 = '0' then
---				  data := data / 2;
---				elsif data mod 2 = '1' then
---				  data := data * 3 + 1;
---				end if;
---			end loop;
